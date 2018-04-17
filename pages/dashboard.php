b078e9c78b4b64f5ea863412d06630efa510f111
vakar4uk <31296706+vakar4uk@users.noreply.github.com>
2018-04-17 16:57:12 -0500

    works

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -2,6 +2,23 @@
     include('navbars_footers/header.php')
 ?>
 
+<div class="main-dashboard container-fluid">
+    <div class="row">
+        <div class="col-lg-4">
+            <a style="color: white;" class="navbar-brand btn float-left" href="landing.php">
+                <i class="fas fa-th-large"></i>
+            </a>  
+        </div>
+        <div class="col-lg-4">
+            <h1 class="lead text-center">HPS Configurator</h1>
+        </div>
+        <div class="col-lg-4">
+            <a style="color: white;" class="navbar-brand navbar-right btn float-right" href="#">
+                <i class="fas fa-cog"></i>
+            </a> 
+        </div>
+    </div>
+</div>
 
 <?php
     include('navbars_footers/footer.php')
