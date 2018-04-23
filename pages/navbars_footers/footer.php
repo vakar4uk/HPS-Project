db3c4e77562968e4e17dbd75db56df3519b2c717
chrisnguyen04 <34491734+chrisnguyen04@users.noreply.github.com>
2018-04-23 15:19:42 -0500

    prototype footer

--- pages/navbars_footers/footer.php
+++ pages/navbars_footers/footer.php
@@ -1,3 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+div.sticky {
+    position: -webkit-sticky;
+    position: sticky;
+    bottom: 0;
+    background-color: turquoise;
+    padding: 5px;
+    font-size: 7px;
+    opacity: 2;
+}
+    
+    .footerbtn {
+    border: none;
+    background-color: inherit;
+    padding: 10px 8px;
+    font-size: 12px;
+    cursor: pointer;
+    display: inline-block;
+    margin-left:1150px
+   
+       
+}
+.big-icon {
+    font-size: 1500px;
+}
+
+.btnhover:hover {background: grey;}
+
+.default {color: whitesmoke;}
+</style>
+</head>
+<body>
+                
+
+<div class="sticky">
+     <button class="footerbtn btnhover default">Mark For Review &nbsp; <i  class="fa fa-check"></i></button>
+    </div>   
+
+                
+                                                                                                                                                                    
         <script src="../app.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
