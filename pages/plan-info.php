2ad103a58460d23f8cbfb272e7e2eaa27eb96a79
charliepan <32108295+charliepan@users.noreply.github.com>
2018-06-21 16:58:08 -0500

    practice
    Might not want to merge into Combining branch

--- pages/plan-info.php
+++ pages/plan-info.php
@@ -20,7 +20,7 @@
 
                 <div class="my-container">
                     <div class="carrier-name">
-                        <p>Plans Code</p><span id="icons"><i class="far fa-envelope fa-1x"></i><i class="fas fa-ellipsis-v fa-1x"></i></span>
+                        <p>Plans Code</p><span id="icons"><i class="far fa-envelope fa-1x"></i><i id="ppr-pc" class="fas fa-ellipsis-v fa-1x"></i></span>
                         <hr id="hr">
                       
                         
@@ -29,7 +29,7 @@
                 
                 <div class="my-container">
                     <div class="carrier-name">
-                        <p>Rates</p><span id="icons"><i class="far fa-envelope fa-1x"></i><i class="fas fa-ellipsis-v fa-1x"></i></span>
+                        <p>Rates</p><span id="icons"><i class="far fa-envelope fa-1x"></i><i id="ppr-r" class="fas fa-ellipsis-v fa-1x"></i></span>
                         <hr id="hr">
                        
                     </div>
@@ -37,7 +37,7 @@
                 
                 <div class="my-container">
                     <div class="carrier-name">
-                        <p>Change Plan</p><span id="icons"><i class="far fa-envelope fa-1x"></i><i class="fas fa-ellipsis-v fa-1x"></i></span>
+                        <p>Change Plan</p><span id="icons"><i class="far fa-envelope fa-1x"></i><i id="ppr-cp" class="fas fa-ellipsis-v fa-1x"></i></span>
                         <hr id="hr">
                   
                     </div>
@@ -45,9 +45,50 @@
                 
                 <div class="my-container">
                     <div class="carrier-name">
-                        <p>Age Rules</p><span id="icons"><i class="far fa-envelope fa-1x"></i><i class="fas fa-ellipsis-v fa-1x"></i></span>
+                        <p>Age Rules</p><span id="icons"><i class="far fa-envelope fa-1x"></i><i id="ppr-ar" class="fas fa-ellipsis-v fa-1x"></i></span>
                         <hr id="hr">
-   
+                        <div class="row">
+                            <div class="container-fluid">
+                                <div class="col-lg-6 select-product">
+                                    <div class="form-group">
+                                        <input list="products" name="product-choice" class="form-control" id="product" placeholder="Select Product" required>
+                                        <datalist id="products">
+                                            <option value="Blue Shield"></option>
+                                            <option value="Florida Blue"></option>
+                                            <option value="United Health Care"></option>
+                                        </datalist>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="container-fluid">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <input type="number" name="min-age" class="form-control" id="min-age" placeholder="Applicant Min Age *" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <input type="number" name="max-age" class="form-control" id="max-age" placeholder="Applicant Max Age *" required>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="container-fluid">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <input type="number" name="c-min-age" class="form-control" id="c-min-age" placeholder="Child Min Age *" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <input type="number" name="c-max-age" class="form-control" id="c-max-age" placeholder="Child Max Age *" required>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
